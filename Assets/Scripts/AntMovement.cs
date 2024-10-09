d8e73c587677122cae07b03eef00c6c675b4bb80
Gooeyduck <johan.liu@hotmail.com>
2024-10-09 04:41:49 +0200

    Hämtmat!!
    Myrorna flyttar sig randomly över tid, och hittar de mat så går de fram till den, tar med den tbx till stacken, sen fortsätter leta efter mat igen!

--- Assets/Scripts/AntMovement.cs
+++ Assets/Scripts/AntMovement.cs
@@ -6,9 +6,15 @@ using UnityEngine.AI;
 public class AntMovement : MonoBehaviour
 {
     private NavMeshAgent agent;
+    public GameObject nest;
     private int count = 0;
     public NavMeshHit hit;
     public GameObject food;
+    private bool foundFood = false;
+    public float detectionRange = 5.0f; // Hur nära myran behöver va för att upptäcka att det finns mat
+    public float moveCooldown = 2.0f; // Tidsintervall för random movement
+    private float time2NextMove = 0.0f; // Trackar när myrar borde flytta sig härnäst
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +26,8 @@ public class AntMovement : MonoBehaviour
     {
         Vector3 randPos = RandPlaneLoc(10.0f);
         agent.SetDestination(randPos);
+        // Resetta timern
+        time2NextMove = moveCooldown;
     }
 
     public Vector3 RandPlaneLoc(float range)
@@ -36,7 +44,52 @@ public class AntMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.destination = food.transform.position;
+        if(!foundFood)
+        {
+            // Decreasa timern med varje frame
+            time2NextMove -= Time.deltaTime;
+
+            // Funktionen kallas bara när myran har nått sin destination och off movement cooldown
+            if(agent.remainingDistance <= agent.stoppingDistance && time2NextMove <= 0.0f)
+            {
+                Move2RandPos();
+            }
+
+            float dist2Food = Vector3.Distance(transform.position, food.transform.position);
+
+            // Om mat hittad
+            if(dist2Food <= detectionRange)
+            {
+                // Gå mot mat
+                agent.destination = food.transform.position;
+            }
+
+            // Om myran är framme vid maten
+            if(dist2Food <= 1.0f)
+            {
+                foundFood = true;
+                Debug.Log("Wooow foowmd!!");
+            }
+        }
+        else
+        {
+            float dist2Nest = Vector3.Distance(transform.position, nest.transform.position);
+            // Gå mot nest
+            agent.destination = nest.transform.position;
+
+            // Om myran är framme vid nest
+            if(dist2Nest <= 1.0f)
+            {
+                // Lämna ifrån maten
+                foundFood = false;
+                Debug.Log("Time 4 chomk! yummy");
+                // Fortsätt leta efter mer mat
+                Move2RandPos();
+            }
+        }
+
+        // Går dirr till maten
+        //agent.destination = food.transform.position;
 
         /* if (count%1000==0)
         {
